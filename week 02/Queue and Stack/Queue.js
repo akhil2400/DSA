b2663ec9bf14eb3a778f595c51f8ea92779babd6
AKHIL KRISHNA <akhilkrishna2400@gmail.com>
2025-02-08 15:11:09 +0530

    asdfghh

--- week 02/Queue and Stack/Queue.js
+++ week 02/Queue and Stack/Queue.js
@@ -1,52 +1,271 @@
-class Queue {
-  constructor() {
-    this.queue = [];
-  }
+// class Queue {
+//   constructor() {
+//     this.queue = [];
+//   }
 
-  enqueue(element) {
-    this.queue.push(element);
-  }
+//   enqueue(element) {
+//     this.queue.push(element);
+//   }
 
-  dequeue() {
-    if (this.isEmpty()) {
-      return "underFlow,cannot perform dequeue";
-    }
-    return this.queue.shift();
-  }
+//   dequeue() {
+//     if (this.isEmpty()) {
+//       return "underFlow,cannot perform dequeue";
+//     }
+//     return this.queue.shift();
+//   }
 
-  isEmpty() {
-    return this.size() === 0
-  }
+//   isEmpty() {
+//     return this.size() === 0
+//   }
 
-  front() {
-    if (this.isEmpty()) {
-      return "No Elements in the Queue";
-    }
-    return this.queue[0];
-  }
-  size() {
-    return this.queue.length;
-  }
+//   front() {
+//     if (this.isEmpty()) {
+//       return "No Elements in the Queue";
+//     }
+//     return this.queue[0];
+//   }
+//   size() {
+//     return this.queue.length;
+//   }
 
-  printQueue() {
-    let QueueString = "";
-    for (let i = 0; i < this.size(); i++) {
-      QueueString += this.queue[i] + ", ";
-    }
-    console.log("Queue: " + QueueString);
+//   printQueue() {
+//     let QueueString = "";
+//     for (let i = 0; i < this.size(); i++) {
+//       QueueString += this.queue[i] + ", ";
+//     }
+//     console.log("Queue: " + QueueString);
 
-  }
-}
-
-
-const myQueue = new Queue();
-myQueue.enqueue(5)
-myQueue.enqueue(96)
-myQueue.enqueue(785)
-myQueue.enqueue(200)
+//   }
+// }
 
 
-myQueue.dequeue()
-myQueue.printQueue()
-console.log(myQueue.front())
-console.log(myQueue.size())
+// const myQueue = new Queue();
+// myQueue.enqueue(5)
+// myQueue.enqueue(96)
+// myQueue.enqueue(785)
+// myQueue.enqueue(200)
+
+
+// myQueue.dequeue()
+// myQueue.printQueue()
+// console.log(myQueue.front())
+// console.log(myQueue.size())
+
+
+//Stack using Queue
+
+// class SUQ {
+//   constructor(){
+//       this.q1= [];
+//       this.q2= [];
+//   }
+  
+//   push(x) {
+//       while(this.q1.length){
+//           this.q2.push(this.q1.shift())
+//       }
+
+//       console.log("Adding..." + x)
+//       this.q1.push(x);
+
+//       while(this.q2.length){
+//           this.q1.push(this.q2.shift());
+//       }
+
+//       console.log("Added.")
+//   }
+
+//   pop(){
+//       console.log("Popping...")
+//       if(this.q1.length === 0) return "Stack is Empty"
+//       return "Popped Value is :" + this.q1.shift();
+
+//   }
+
+//   top(){
+//       console.log("Searching for top...")
+//   return this.q1.length > 0 ? "Top element is :" + this.q1[0]:"Stack is Empty"
+//   }
+//   isEmpty(){
+//       console.log("checking the stack is empty or not")
+//       return this.q1.length === 0;
+//   }
+// }
+// let Stack = new SUQ();
+// Stack.push(10);
+// Stack.push(20);
+// Stack.push(30);
+// console.log("Contents in the stack")
+
+// console.log(Stack.q1)
+// console.log(Stack.pop())
+// console.log(Stack.top())
+// console.log(Stack.isEmpty())
+// console.log(Stack.pop())
+// console.log(Stack.pop())
+// console.log(Stack.pop())
+
+
+//Reversing a string using Queue
+
+// class Queue {
+//   constructor(){
+//       this.val = [];
+//   }
+  
+//   enqueue(elem) {
+//       this.val.push(elem)
+//   }
+//   dequeue(){
+//       if(this.isEmpty()) return "Queue is empty";
+//       return this.val.shift();
+//   }
+  
+//   isEmpty() {
+//       return this.val.length ===0;
+//   }
+  
+// }
+
+// function reverse(str){
+//   let queue = new Queue();
+  
+//   for(let char of str){
+//       queue.enqueue(char);
+//   }
+  
+//   let stack = [];
+  
+//   while(!queue.isEmpty()) {
+//       stack.push(queue.dequeue());
+//   }
+  
+//   let reversedStr = "";
+  
+//   while(stack.length) {
+//       reversedStr += stack.pop();
+//   }
+  
+//   return reversedStr;
+// }
+
+// let str = "Akhil"
+// console.log(str)
+// console.log(reverse(str));
+
+
+// class Queue{
+//   constructor(){
+//     this.queue=[]
+//   }
+
+//   enqueue(elem){
+//     this.queue.push(elem);
+//   }
+
+//   dequeue(){
+//     if(this.isEmpty()) return "The Queue is Empty"
+//     return this.queue.shift();
+//   }
+
+//   isEmpty(){
+//     return this.queue.length === 0;
+//   }
+
+//   top(){
+//     if(this.isEmpty()) return "The Queue is Empty"
+
+//     return this.queue[0];
+//   }
+
+//   print(){
+//     let Queue = "";
+//     for(let i = 0 ; i<this.queue.length ; i++){
+//       Queue += this.queue[i]+"->" 
+//     }
+//     console.log("Queue : " + Queue)
+//   }
+// }
+
+// let myQueue = new Queue();
+
+// myQueue.enqueue(10)
+// myQueue.enqueue(20)
+// myQueue.enqueue(40)
+// myQueue.enqueue(70)
+
+// console.log(myQueue.top())
+// myQueue.print()
+
+// myQueue.dequeue()
+
+// myQueue.print()
+
+// console.log(myQueue.top());
+
+
+//second largest using Queue
+
+// class Queue{
+//   constructor(){
+//     this.queue=[]
+//   }
+
+//   enqueue(elem){
+//     this.queue.push(elem);
+//   }
+
+//   dequeue(){
+//     if(this.isEmpty()) return "The Queue is Empty"
+//     return this.queue.shift();
+//   }
+
+//   isEmpty(){
+//     return this.queue.length === 0;
+//   }
+
+//   top(){
+//     if(this.isEmpty()) return "The Queue is Empty"
+
+//     return this.queue[0];
+//   }
+
+//   print(){
+//     let Queue = "";
+//     for(let i = 0 ; i<this.queue.length ; i++){
+//       Queue += this.queue[i]+"->" 
+//     }
+//     console.log("Queue : " + Queue)
+//   }
+//   secondSmallest() {
+//     if(this.queue.length < 2) return "No enough elements in the arr"
+
+//     let smallest = Infinity;
+//     let secondSmallest = Infinity;
+
+//     for(let num of this.queue){
+//       if(num < smallest){
+//         secondSmallest = smallest;
+//         smallest = num;
+//       }else if(num > smallest && num < secondSmallest){
+//         secondSmallest = num;
+//       }
+//     }
+//     return (secondSmallest === Infinity)?"No secondSmallest in this":secondSmallest
+    
+//   }
+// }
+
+// let myQueue = new Queue();
+
+// myQueue.enqueue(10)
+// myQueue.enqueue(20)
+// myQueue.enqueue(40)
+// myQueue.enqueue(70)
+
+// myQueue.print()
+
+
+// console.log(myQueue.secondSmallest())
+
+
--- week 02/Queue and Stack/Stack.js
+++ week 02/Queue and Stack/Stack.js
@@ -1,5 +1,7 @@
-// // /*================= STACK =================*/
+/*================= STACK =================*/
 
+/*Stack Implimentation
+======================*/
 
 // class stack{
 //   constructor(){
@@ -75,168 +77,174 @@
 
 
 
-// //creating ll using stack;
+/*creating ll using stack;
+==========================
 
-// //using an Array-Based stack;
+using an Array-Based stack;
+---------------------------*/
 
-// // class stack{
-// //   constructor(){
-// //     this.stack = [];
-// //   }
+// class stack{
+//   constructor(){
+//     this.stack = [];
+//   }
 
-// //   push(value){
-// //     this.stack.push(value);
-// //   }
+//   push(value){
+//     this.stack.push(value);
+//   }
 
-// //   pop(){
-// //     return this.stack.pop();
-// //   }
+//   pop(){
+//     return this.stack.pop();
+//   }
 
-// //   peek(){
-// //     return this.stack[this.stack.length-1];
-// //   }
+//   peek(){
+//     return this.stack[this.stack.length-1];
+//   }
 
-// //   isEmpty(){
-// //     return this.stack.length === 0;
-// //   }
+//   isEmpty(){
+//     return this.stack.length === 0;
+//   }
 
-// //   size(){
-// //     return this.stack.length
-// //   }
-// // }
+//   size(){
+//     return this.stack.length
+//   }
+// }
 
-// // class ListNode{
-// //   constructor(value){
-// //     this.value = value;
-// //     this.next = null
-// //   }
-// // }
+// class ListNode{
+//   constructor(value){
+//     this.value = value;
+//     this.next = null
+//   }
+// }
 
-// // class LinkedList{
-// //   constructor(){
-// //     this.stack = new stack();
-// //     this.head = null;
-// //   }
-
-
-// //   push(value){
-// //     let newNode = new ListNode(value)
-// //     if(this.head){
-// //       newNode.next = this.head;
-// //     }
-// //     this.head = newNode;
-// //     this.stack.push(newNode);
-// //   }
-
-// //   pop(){
-// //     if(this.head){
-// //       this.stack.pop();
-// //       this.head = this.head.next;
-// //     }
-// //   }
-
-// //   print(){
-// //     let current = this.head;
-// //     let result = "";
-// //     while(current){
-// //       result += current.value + "->";
-// //       current = current.next;
-// //     }
-// //     console.log(result+"null");
-// //   }
-// // }
+// class LinkedList{
+//   constructor(){
+//     this.stack = new stack();
+//     this.head = null;
+//   }
 
 
-// // const ll = new LinkedList();
-// // ll.push(10);
-// // ll.push(20);
-// // ll.push(30);
-// // ll.print()
+//   push(value){
+//     let newNode = new ListNode(value)
+//     if(this.head){
+//       newNode.next = this.head;
+//     }
+//     this.head = newNode;
+//     this.stack.push(newNode);
+//   }
+
+//   pop(){
+//     if(this.head){
+//       this.stack.pop();
+//       this.head = this.head.next;
+//     }
+//   }
+
+//   print(){
+//     let current = this.head;
+//     let result = "";
+//     while(current){
+//       result += current.value + "->";
+//       current = current.next;
+//     }
+//     console.log(result+"null");
+//   }
+// }
 
 
-// // class Node {
-// //   constructor(value) {
-// //     this.value = value;
-// //     this.next = null;
-// //   }
-// // }
+// const ll = new LinkedList();
+// ll.push(10);
+// ll.push(20);
+// ll.push(30);
+// ll.print()
 
-// // class Stack {
-// //   constructor() {
-// //     this.top = null;
-// //   }
-// //   push(value) {
-// //     let newNode = new Node(value)
-// //     newNode.next = this.top;
-// //     this.top = newNode;
-// //   }
-// //   pop() {
-// //     if (!this.top) return null;
-// //     let poppedNode = this.top;
-// //     this.top = this.top.next;
-// //     return poppedNode.value;
-// //   }
-// //   peek() {
-// //     return this.top ? this.top.value : null;
-// //   }
-// //   isEmpty() {
-// //     return this.top = null;
-// //   }
-// //   print() {
-// //     let current = this.top;
-// //     let result = "";
-// //     while (current) {
-// //       result += current.value + "->";
-// //       current = current.next;
-// //     }
-// //     console.log(result + "null");
-// //   }
-// // }
-// // const stack = new Stack();
-// // stack.push(10);
-// // stack.push(20);
-// // stack.push(30);
-// // stack.push(40);
-// // stack.print();
+/*ll as stack
+==============*/
 
-// // stack.pop();
-// // stack.print();
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
 
+// class Stack {
+//   constructor() {
+//     this.top = null;
+//   }
+//   push(value) {
+//     let newNode = new Node(value)
+//     newNode.next = this.top;
+//     this.top = newNode;
+//   }
+//   pop() {
+//     if (!this.top) return null;
+//     let poppedNode = this.top;
+//     this.top = this.top.next;
+//     return poppedNode.value;
+//   }
+//   peek() {
+//     return this.top ? this.top.value : null;
+//   }
+//   isEmpty() {
+//     return this.top = null;
+//   }
+//   print() {
+//     let current = this.top;
+//     let result = "";
+//     while (current) {
+//       result += current.value + "->";
+//       current = current.next;
+//     }
+//     console.log(result + "null");
+//   }
+// }
+// const stack = new Stack();
+// stack.push(10);
+// stack.push(20);
+// stack.push(30);
+// stack.push(40);
+// stack.print();
 
-// // REVERSE A STRING USING STACK
-
-// // class Stack {
-// //   constructor() {
-// //     this.stack = []
-// //   }
-
-// //   push(char) {
-// //     this.stack.push(char)
-// //   }
-// //   pop() {
-// //     return this.stack.pop()
-// //   }
-// //   isEmpty() {
-// //     return this.stack.length === 0
-// //   }
-// // }
-
-// // function reverse(str) {
-// //   let stack = new Stack()
-// //   for (let char of str) {
-// //     stack.push(char)s
-// //   }
-// //   let rev = ""
-// //   while (!stack.isEmpty()) {
-// //     rev += stack.pop()
-// //   }
-// //   return rev
-// // }
-// // let str = "shamil"
-// // console.log(reverse(str))
+// stack.pop();
+// stack.print();
 
 
-// //QUEUE USING STACK
+/* REVERSE A STRING USING STACK
+=============================*/
+
+// class Stack {
+//   constructor() {
+//     this.stack = []
+//   }
+
+//   push(char) {
+//     this.stack.push(char)
+//   }
+//   pop() {
+//     return this.stack.pop()
+//   }
+//   isEmpty() {
+//     return this.stack.length === 0
+//   }
+// }
+
+// function reverse(str) {
+//   let stack = new Stack()
+//   for (let char of str) {
+//     stack.push(char)
+//   }
+//   let rev = ""
+//   while (!stack.isEmpty()) {
+//     rev += stack.pop()
+//   }
+//   return rev
+// }
+// let str = "shamil"
+// console.log(reverse(str))
+
+
+/*QUEUE USING STACK
+==================*/
 
 // class Stack {
 //   constructor() {
